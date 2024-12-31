bd2f973d6802d81ba4c1bb8cbff8f0f2f08d6d78
ETIENNE_JOUAULT <etienne@studiorelief.fr>
2024-12-31 14:17:00 +0100

    commit following review customer

--- src/index.ts
+++ src/index.ts
@@ -9,6 +9,7 @@ import {
   hideEmptyLabelsContainer,
 } from '$utils/catalogue/catalogueQuickFix';
 import { formInputLabels, showSiret } from '$utils/contact/formInput';
+import { filterCooperativesByCountry } from '$utils/coop/coop';
 import { loadScript } from '$utils/global/loadScript';
 import { navScroll, setCurrentDropdownState, setCurrentRowFromURL } from '$utils/global/navbar';
 import {
@@ -91,6 +92,9 @@ window.Webflow.push(() => {
   }
   /* swiper */
   swiperChronologie();
+
+  /* TEST COOP */
+  filterCooperativesByCountry();
 
   //Home
   swiperHpPicture();
