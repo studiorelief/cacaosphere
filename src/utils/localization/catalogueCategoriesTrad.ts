8ffeec749c1f33c24a5afccafdfbc3f47daf17dc
Clément Murzeau <clement@studiorelief.fr>
2025-04-06 18:36:49 +0200

    Update catalogueCategoriesTrad
    -> Ajout des attributs sur page produits pour filtring
    -> Ajout d'un listener si changement d'URL

--- src/utils/localization/catalogueCategoriesTrad.ts
+++ src/utils/localization/catalogueCategoriesTrad.ts
@@ -1,25 +1,44 @@
 export function catalogueCategoriesTrad() {
-  if (window.location.pathname.includes('/en/')) {
-    const categoryElements = document.querySelectorAll('[localization=categorie]');
-    categoryElements.forEach((element: Element) => {
-      const text = element.textContent;
+  const translateCategories = () => {
+    if (window.location.pathname.includes('/en/')) {
+      const categoryElements = document.querySelectorAll('[localization=categorie]');
+      categoryElements.forEach((element: Element) => {
+        const text = element.textContent;
 
-      if (text) {
-        switch (text.trim()) {
-          case 'couvertures pure origine':
-            element.textContent = 'Pure origin covers';
-            break;
-          case "chocolats d'atelier":
-            element.textContent = 'Workshop chocolates';
-            break;
-          case 'fèves':
-            element.textContent = 'Feves';
-            break;
-          case 'ingrédients':
-            element.textContent = 'Ingredients';
-            break;
+        if (text) {
+          switch (text.trim()) {
+            case 'couvertures pure origine':
+              element.textContent = 'Pure Couverture chocolates';
+              break;
+            case "chocolats d'atelier":
+              element.textContent = 'Workshop chocolates';
+              break;
+            case 'fèves':
+              element.textContent = 'Cocoa Beans';
+              break;
+            case 'FEVES':
+              element.textContent = 'Cocoa Beans';
+              break;
+            case 'ingrédients':
+              element.textContent = 'Key Ingredients';
+              break;
+          }
         }
-      }
-    });
-  }
+      });
+    }
+  };
+
+  // Initial translation
+  translateCategories();
+
+  // Listen for URL changes
+  const observer = new MutationObserver(() => {
+    translateCategories();
+  });
+
+  // Observe changes to the URL
+  observer.observe(document.querySelector('body')!, {
+    childList: true,
+    subtree: true,
+  });
 }
